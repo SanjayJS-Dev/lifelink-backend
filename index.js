2b26c7bc93b2047e1bd55cc8e642964753a8deac
SanjayJS-Dev <sanjaykrishnadas.online@gmail.com>
2024-07-04 16:19:42 +0530

    added .env file and packages

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = process.env.PORT
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/localities', (req, res) => {
+  
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(port, ()=>{
+  console.log("Server is running on Port Number: "+port)
 })
