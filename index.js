18817168c8df3790d370b21c0bd2d5c8679054dd
SanjayJS-Dev <sanjaykrishnadas.online@gmail.com>
2024-07-06 22:38:53 +0530

    get data from token added

--- index.js
+++ index.js
@@ -14,116 +14,133 @@ app.use(express.json())
 app.use(cors())
 
 //authentication using jwt
-const verifyToken = (req,res,next) => {
-  let authHeader = req.headers.authorization
-  if(authHeader == undefined) {
-    res.status(401).json({message:"No Token. Authentication Failed"})
-  } else {
-    let token = authHeader.split(" ")[1]
-    jwt.verify(token,process.env.JWT_KEY,(err,decoded)=>{
-      if(err) {
-        res.status(401).json({message:"Invalid Token. Authentication Failed"})
-      } else {
-        next();
-      }
-    })
-  }
+const verifyToken = (req, res, next) => {
+    let authHeader = req.headers.authorization
+    if (authHeader == undefined) {
+        res.status(401).json({ message: "No Token. Authentication Failed" })
+    } else {
+        let token = authHeader.split(" ")[1]
+        jwt.verify(token, process.env.JWT_KEY, (err, decoded) => {
+            if (err) {
+                res.status(401).json({ message: "Invalid Token. Authentication Failed" })
+            } else {
+                next();
+            }
+        })
+    }
 }
 
+//get institution data from token
+app.get('/getInstData', (req, res) => {
+    let authHeader = req.headers.authorization
+    if (authHeader == undefined) {
+        res.status(401).json({ message: "No Token. Authentication Failed" })
+    } else {
+        let token = authHeader.split(" ")[1]
+        jwt.verify(token, process.env.JWT_KEY, (err, decoded) => {
+            if (err) {
+                res.status(401).json({ message: "Invalid Token. Authentication Failed" })
+            } else {
+                res.status(200).json(decoded)
+            }
+        })
+    }
+})
+
 app.get('/localities', async (req, res) => {
-  const Localities = await Locality.find({})
-  res.status(200).json(Localities)
+    const Localities = await Locality.find({})
+    res.status(200).json(Localities)
 })
 
 app.post('/addVolunteer', async (req, res) => {
-  let { name, gender, dob, bgrp, locality, mobile, address, password } = req.body
-  let checkVolunteer = await Volunteer.findOne({ mobile: mobile })
-  if (checkVolunteer) {
-    res.status(409).json({ message: "Mobile Number already Registered" })
-    return
-  }
-  const salt = bcrypt.genSaltSync(10)
-  const hash = bcrypt.hashSync(password, salt)
-  const volunteer = new Volunteer({
-    name: name,
-    gender: gender,
-    dob: dob,
-    bgrp: bgrp,
-    locality: locality,
-    mobile: mobile,
-    address: address,
-    password: hash,
-    verfied: false,
-    last: new Date()
-  })
-  await volunteer.save()
-    .then(() => res.status(200).json({ message: "Volunteer Registration Successfull" }))
-    .catch((error) => res.json({ message: error.message }))
+    let { name, gender, dob, bgrp, locality, mobile, address, password } = req.body
+    let checkVolunteer = await Volunteer.findOne({ mobile: mobile })
+    if (checkVolunteer) {
+        res.status(409).json({ message: "Mobile Number already Registered" })
+        return
+    }
+    const salt = bcrypt.genSaltSync(10)
+    const hash = bcrypt.hashSync(password, salt)
+    const volunteer = new Volunteer({
+        name: name,
+        gender: gender,
+        dob: dob,
+        bgrp: bgrp,
+        locality: locality,
+        mobile: mobile,
+        address: address,
+        password: hash,
+        verfied: false,
+        last: new Date()
+    })
+    await volunteer.save()
+        .then(() => res.status(200).json({ message: "Volunteer Registration Successfull" }))
+        .catch((error) => res.json({ message: error.message }))
 })
 
 app.post('/addInstitution', async (req, res) => {
-  let { name, locality, phone, address, email, password } = req.body
-  let checkInstitution = await Institution.findOne({ phone: phone })
-  if (checkInstitution) {
-    res.status(409).json({ message: "Mobile Number already Registered" })
-    return
-  }
-  checkInstitution = await Institution.findOne({ email:email })
-  if(checkInstitution) {
-    res.status(409).json({ message: "Email ID already Registered" })
-    return
-  }
-  const salt = bcrypt.genSaltSync(10)
-  const hash = bcrypt.hashSync(password, salt)
-  const institution = new Institution({
-    name: name,
-    locality: locality,
-    phone: phone,
-    address: address,
-    email: email,
-    password: hash,
-    verfied: false,
-    location: {
-      latitude:"0",
-      longitude:"0"
+    let { name, locality, phone, address, email, password } = req.body
+    let checkInstitution = await Institution.findOne({ phone: phone })
+    if (checkInstitution) {
+        res.status(409).json({ message: "Mobile Number already Registered" })
+        return
     }
-  })
-  await institution.save()
-    .then(() => res.status(200).json({ message: "Institution Registration Successfull" }))
-    .catch((error) => res.json({ message: error.message }))
+    checkInstitution = await Institution.findOne({ email: email })
+    if (checkInstitution) {
+        res.status(409).json({ message: "Email ID already Registered" })
+        return
+    }
+    const salt = bcrypt.genSaltSync(10)
+    const hash = bcrypt.hashSync(password, salt)
+    const institution = new Institution({
+        name: name,
+        locality: locality,
+        phone: phone,
+        address: address,
+        email: email,
+        password: hash,
+        verfied: false,
+        location: {
+            latitude: "0",
+            longitude: "0"
+        }
+    })
+    await institution.save()
+        .then(() => res.status(200).json({ message: "Institution Registration Successfull" }))
+        .catch((error) => res.json({ message: error.message }))
 })
 
-app.post('/validateLogin', async (req,res) => {
-  let { email,password } = req.body
-  let institution = await Institution.findOne({email:email})
-  if(institution) {
-    if(bcrypt.compareSync(password,institution.password)) {
-      if(institution.verfied){
-        let token = jwt.sign(institution.toJSON(),process.env.JWT_KEY)
-        res.status(200).json(token)
-      } else {
-        res.status(401).json({message:"Registration not Verified"})
-      }
+app.post('/validateLogin', async (req, res) => {
+    let { email, password } = req.body
+    let institution = await Institution.findOne({ email: email })
+    if (institution) {
+        if (bcrypt.compareSync(password, institution.password)) {
+            if (institution.verfied) {
+                let token = jwt.sign(institution.toJSON(), process.env.JWT_KEY)
+                res.status(200).json(token)
+            } else {
+                res.status(401).json({ message: "Registration not Verified" })
+            }
+        } else {
+            res.status(401).json({ message: "Incorrect Password" })
+        }
     } else {
-      res.status(401).json({message:"Incorrect Password"})
+        res.status(401).json({ message: "Invalid Email ID" })
     }
-  } else {
-    res.status(401).json({message:"Invalid Email ID"})
-  }
 })
 
 app.post('/isVolunteer', async (req, res) => {
-  let { mobile, password } = req.body
-  let volunteer = await Volunteer.findOne({mobile:mobile})
-  if(volunteer) {
-    if(bcrypt.compareSync(password,volunteer.password)) {
-        res.status(200).json(volunteer)
+    let { mobile, password } = req.body
+    let volunteer = await Volunteer.findOne({ mobile: mobile })
+    if (volunteer) {
+        if (bcrypt.compareSync(password, volunteer.password)) {
+            res.status(200).json(volunteer)
+        } else {
+            res.status(401).json({ message: "Incorrect Password" })
+        }
     } else {
-        res.status(401).json({message:"Incorrect Password"})
+        res.status(401).json({ message: "Invalid Mobile Number" })
     }
-  } else {
-    res.status(401).json({message:"Invalid Mobile Number"})
-  }
 })
 
 app.listen(port)
