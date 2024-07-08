2e1b76e9b9ee6e288950a64e33c87c599c7ccbf5
SanjayJS-Dev <sanjaykrishnadas.online@gmail.com>
2024-07-08 20:24:47 +0530

    updated methods for apis

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const app = express()
 app.use(express.json())
 app.use(cors({
     origin:"http://localhost:5173",
-    methods: ['GET','POST'],
+    methods: ['GET','POST','PATCH','DELETE'],
     credentials: true
 }))
 
@@ -24,7 +24,7 @@ const server = http.createServer(app)
 const socketServer = socket(server,{
     cors: {
         origin: "http://localhost:5173",
-        methods: ['GET','POST'],
+        methods: ['GET','POST','PATCH','DELETE'],
         credentials: true
     }
 })
